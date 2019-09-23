48ac5ef68a77805fde4f40a9c8e9c56d1e9d8570
Razaqa Dhafin <dhafins18@gmail.com>
2019-09-23 22:48:03 +0700

    Add main dashboard page template

---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layout/dashboard/base')
+
+@section('title', 'Welcome')
+
+@section('extra-fonts')
+
+@endsection
+
+@section('prerender-js')
+
+@endsection
+
+@section('extra-css')
+
+@endsection
+
+@section('content')
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Dashboard
+      <small>Control panel</small>
+    </h1>
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+
+</section>
+</div>
+@endsection
+
+@section('extra-js')
+
+@endsection
