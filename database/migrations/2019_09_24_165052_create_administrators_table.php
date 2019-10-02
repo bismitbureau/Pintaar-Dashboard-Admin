14937a761342dba38bd5aeba464629451c2f156e
Unknown <dhafins18@gmail.com>
2019-10-02 08:49:46 +0700

    Setup for auth migrations

--- database/migrations/2019_09_24_165052_create_administrators_table.php
+++ database/migrations/2019_09_24_165052_create_administrators_table.php
@@ -14,8 +14,13 @@ class CreateAdministratorsTable extends Migration
     public function up()
     {
         Schema::create('administrators', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+          $table->bigIncrements('id');
+          $table->string('name');
+          $table->string('email')->unique();
+          $table->timestamp('email_verified_at')->nullable();
+          $table->string('password');
+          $table->rememberToken();
+          $table->timestamps();
         });
     }
 
